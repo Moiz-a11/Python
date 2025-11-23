6097bbe114dee60955f48411b3d9cd8ad3b248a4
moiz shaikh <moeez4286@gmail.com>
2025-11-23 17:52:08 +0530

    practice

---
+++ conditionals/program1.py
@@ -0,0 +1,52 @@
+
+# age = float(input("enter age : "))
+# if age>=18:
+#    print("you can vote")
+#    print("you can drive also")
+# else:
+#    print("you are not addult")
+
+# color  = input("enter colour ")
+# if color== "green": 
+#       print("you are on")
+# elif color =="red":
+#      print("stop here")
+
+# elif color =="yellow":
+#      print("wait for people ")
+# else:
+#      print("wrong color")
+
+# age = int(input("enter age : "))
+
+# if age <13 :
+#    print("child")
+# elif age>=13 and age<=19:
+#    print("teenage")
+# elif age>=20:
+#      print("man")
+# else:
+#     print("invalid age")
+
+# username = "admin"
+# password = "pass"
+
+# name = input("enter name : ")
+# code = input("enter password :")
+# if name!=username:
+#    print("wrong username")
+# elif code!=password:
+#      print("invalid password")
+# else:
+#     print("welcome back ")
+
+num = int(input("enter number : "))
+if num%5==0:
+
+   print(num , "is multiple of 5")
+else:
+   print("not multiple of 5")
+
+
+
+
