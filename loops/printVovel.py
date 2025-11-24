fea3ddd4d2875cc8cef5405bf4794b3cac177ebb
moiz shaikh <moeez4286@gmail.com>
2025-11-24 15:50:17 +0530

    loops practice

---
+++ loops/printVovel.py
@@ -0,0 +1,7 @@
+
+word = "artifitial"
+count=0
+for vovel in word:
+    if(vovel=='a' or vovel=='e' or vovel=='i' or vovel=='o' or vovel=='u'):
+        count+=1
+        print(count)
---
+++ rangeFunction.py
@@ -0,0 +1,9 @@
+# range() function generates a sequence of numbers
+# for i in range(5):
+#     print(i)
+
+# for i in range(1,5):
+#     print(i)
+
+# for i in range(2,10,2): 
+#     print(i)
