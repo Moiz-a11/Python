f8df2ddf7daa536c5081f542de7b7675fbaef7ee
moiz shaikh <moeez4286@gmail.com>
2025-11-25 17:52:58 +0530

    string

---
+++ string/sample.py
@@ -0,0 +1,14 @@
+
+word = "ayesha"
+# word2= "moiz"
+# #print(len(word))
+# print(word+ " "+ word2)
+
+
+# print(word[0])
+#strings are immutable >> means we can not change its value by assining values
+
+print(word[3:5]) # slicing the string
+
+#reverse slicing
+print(word[-5:-2])
