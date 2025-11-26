f2e95e822c71b99ded63ece35c53c89078ab8e91
moiz shaikh <moeez4286@gmail.com>
2025-11-26 20:38:21 +0530

    practice question

---
+++ Touples/Q1.py
@@ -0,0 +1,22 @@
+#Q] list all unique courses
+info=[
+    ("sk","math"),
+    ("patel","iqc"),
+    ("ansari","english"),
+    ("pathan","oop"),
+    ("rehan","english")
+]
+# newSet=set()
+# for i in info:
+#   newSet.add(i[1])
+
+
+# print(newSet) 
+    
+  #Q.2] list who inrolled in english
+
+for i in info:
+    if(i[1]=="english"):
+        print(i)
+ # Q.3] create dictionary  (student , set of courses)
+ 
