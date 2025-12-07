01952294a4a1eac010c4c92430a190254a972eb0
moiz shaikh <moeez4286@gmail.com>
2025-12-07 19:09:24 +0530

    problem solving

--- Assignment_Problems/Assignment1.py
+++ Assignment_Problems/Assignment1.py
@@ -1,43 +1,126 @@
+# #Assignment 1
+# #Q.1]
 
-#Q.1]
+# name = input("enter  name :")
+# age = int(input("enter age  :"))
 
-name = input("enter  name :")
-age = int(input("enter age  :"))
+# print(f"Hello {name} you are {age} years old !")
 
-print(f"Hello {name} you are {age} years old !")
+# #Q.2]
 
-#Q.2]
+# num1 = int(input("enter num1 : "))
+# num2 = float(input("enter num2 :"))
+# sum = num1+num2
+# difference = num1-num2
+# product = num1*num2
+# quotiont= num1/num2
 
-num1 = int(input("enter num1 : "))
-num2 = float(input("enter num2 :"))
-sum = num1+num2
-difference = num1-num2
-product = num1*num2
-quotiont= num1/num2
+# print(f"sum = {sum} , diff = {difference} , product = {product} , quotiont = {quotiont}")
 
-print(f"sum = {sum} , diff = {difference} , product = {product} , quotiont = {quotiont}")
+# # Q.3]
+# num1 = int(input("enter 1st integer :"))
+# num2 = int(input("enter 2nd integer :"))
+# num3 = float(input("enter 3rd integer : "))
+# float_num1 = float(num1)
+# float_num2 = float(num2)
+# sum = num1+num2+num3
+# average = sum/3
+# print(f"average = {average}")
 
-# Q.3]
-num1 = int(input("enter 1st integer :"))
-num2 = int(input("enter 2nd integer :"))
-num3 = float(input("enter 3rd integer : "))
-float_num1 = float(num1)
-float_num2 = float(num2)
-sum = num1+num2+num3
-average = sum/3
-print(f"average = {average}")
+# #Q.4]
+# num_str = input("enter num as str :")
+# int_num= int(num_str)
+# float_num=float(num_str)
+# str_num=str(num_str)
 
-#Q.4]
-num_str = input("enter num as str :")
-int_num= int(num_str)
-float_num=float(num_str)
-str_num=str(num_str)
-
-print(type(int_num))
-print(type(float_num))
-print(type(str_num))
+# print(type(int_num))
+# print(type(float_num))
+# print(type(str_num))
 
 
-#Q.5]
-x= 10 + 3 * 2 ** 2
-print(x)
+# #Q.5]
+# x= 10 + 3 * 2 ** 2
+# print(x)
+
+#Assignment 2
+
+#Q 1]
+
+# salary = int(input("enter salary : "))
+
+# if salary<30000:
+#     print("5% tax")
+# elif salary>30000 and salary<=70000:
+#     print("15% tax")
+
+# elif salary>70000:
+#       print("25% tax")
+# else:
+#      print("not specified:")
+
+#Q 2]
+# def print_even_num(a,b):
+#     for num in range(a,b):
+#         if(num%2==0):
+#             print(num)   
+
+# print_even_num(0,10)
+
+#Q 3]
+# count=0
+# def digits_of_num(n):
+#     global count
+#     while n>0:
+#         digit=n%10
+#         print(digit)
+#         n=n//10  #         n=n//10 gives integer value and   n=n/10 gives float lead to break loop
+#         count +=1
+
+
+
+# digits_of_num(312)
+# print("\n")
+# print("count = ",count)
+
+#Q4] returns sum of digits in n
+# def sum_of_digits(n):
+#     total=0
+#     while n>0:
+#         digit = n%10
+#         total += digit
+
+#         n=n//10
+#     return total   
+# print(sum_of_digits(555))
+
+#Q5] divisible by 3 and 5
+
+# for num in range(0,100):
+#     if num % 3 == 0 and num%5 == 0:
+#         print(num)
+
+#Q6]
+
+# while True:
+#     user_input =input("enter number or type Quit : ")
+
+#     if user_input=="Quit":
+#         break
+
+#     num= int(user_input)
+
+#     if num>0:
+#         print("num is positive")
+
+#     elif num<0:
+#            print("num is negative")
+#     else:
+#          print("zero")
+
+
+#Q7]
+
+def is_prime(n):
+
+    for i in range(2,n):
+        if()
